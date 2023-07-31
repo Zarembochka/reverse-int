5fc42fe6fcc1b5a464246c1a3ec98badd7927a6a
Luba Hadunova <luba.godunova2018@gmail.com>
2023-07-31 07:55:36 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+
+    if (n < 10) {
+        return n;
+    }
+
+    let result = 0;
+
+    while (n >= 1) {
+        let lastDigit = n % 10;
+        result = result * 10 + lastDigit;
+        n = Math.floor(n / 10);
+    }
+
+    return result;
 }
